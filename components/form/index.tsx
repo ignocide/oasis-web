75f992c4bc2f5aecf2a08e9c4e610e85f84cbb6d
ignocide <ignocide@gmail.com>
2019-03-28 09:42:24 +0900

    panel, form(input,button) 기본 모듈 추가

---
+++ components/form/index.tsx
@@ -0,0 +1,34 @@
+import Icon from "../common/icon";
+
+const Button = ({ className = '', shape = '', color = '', type = 'text', children, ...props }) => {
+
+    const classNameList = ['button'];
+
+    if (className) {
+        classNameList.push(className)
+    }
+    if (shape) {
+        classNameList.push(`btn-${shape}`)
+    }
+    if (color) {
+        classNameList.push(`btn-${color}`)
+    }
+    return <button className={classNameList.join(' ')} type={type} {...props}>{children}</button>
+}
+
+const IconButton = ({ className = '', type = 'text', shape = '', color = '', name, children, ...props }) => {
+    const classNameList = ['button icon-button'];
+
+    if (className) {
+        classNameList.push(className)
+    }
+    if (shape) {
+        classNameList.push(`btn-${shape}`)
+    }
+    if (color) {
+        classNameList.push(`btn-${color}`)
+    }
+    return <button className={classNameList.join(" ")} type={type} {...props}><Icon name={name} /></button>
+}
+
+export { Button, IconButton }
