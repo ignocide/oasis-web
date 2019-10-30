f34e0ae52a8bdc517ac4adc3c7961d37d4a9be02
sungmin kim <ignocide@gmail.com>
2019-10-30 17:59:40 +0900

    edit fetchByLocation's return type

--- src/api/server/common/weather.ts
+++ src/api/server/common/weather.ts
@@ -8,6 +8,11 @@ interface IWeatherResponse {
   location: ISimpleLocation | SimpleLocation
 }
 
+interface IWeatherAndLocation {
+  weather: WeatherInfo,
+  location: SimpleLocation
+}
+
 class WeatherRepository {
   axios: AxiosInstance;
 
@@ -15,7 +20,7 @@ class WeatherRepository {
     this.axios = instance;
   }
 
-  async fetchByLocation(lat: number, lng: number): Promise<IWeatherResponse> {
+  async fetchByLocation(lat: number, lng: number): Promise<IWeatherAndLocation> {
     const result: any = await instance.get(`/life/weather/location`, {
       params: {
         lat, lng
@@ -31,5 +36,5 @@ class WeatherRepository {
 
 }
 
-export { IWeatherResponse }
+export { IWeatherResponse, IWeatherAndLocation }
 export default new WeatherRepository();
