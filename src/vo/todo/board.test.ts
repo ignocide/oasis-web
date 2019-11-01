1b1c2c20091f3c525612badec1bbd3e88dc0e6f5
sungmin kim <ignocide@gmail.com>
2019-11-01 16:40:16 +0900

    add board,task test case

---
+++ src/vo/todo/board.test.ts
@@ -0,0 +1,24 @@
+import Board from './board';
+
+describe('vo/todo/board', () => {
+  test('constructor', () => {
+    let mock = {
+      id: 1,
+      userId: 1,
+      deleted: false,
+      name: '프로젝트 제목',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    };
+
+    let board = new Board(mock);
+
+
+    expect(board.id).toEqual(mock.id);
+    expect(board.userId).toEqual(mock.userId);
+    expect(board.deleted).toEqual(mock.deleted);
+    expect(board.name).toEqual(mock.name);
+    expect(board.createdAt).toEqual(mock.createdAt);
+    expect(board.updatedAt).toEqual(mock.updatedAt);
+  });
+});
---
+++ src/vo/todo/task.test.ts
@@ -0,0 +1,27 @@
+import Task from './task';
+
+describe('vo/todo/task', () => {
+  test('constructor', () => {
+    let mock = {
+      id: 1,
+      boardId: 1,
+      detail: '상세내용',
+      name: '테스크 제목',
+      step: "TODO",
+      deleted: false,
+      createdAt: new Date,
+      updatedAt: new Date,
+    };
+
+    let task = new Task(mock);
+
+
+    expect(task.id).toEqual(mock.id);
+    expect(task.boardId).toEqual(mock.boardId);
+    expect(task.deleted).toEqual(mock.deleted);
+    expect(task.name).toEqual(mock.name);
+    expect(task.detail).toEqual(mock.detail)
+    expect(task.createdAt).toEqual(mock.createdAt);
+    expect(task.updatedAt).toEqual(mock.updatedAt);
+  });
+});
